d4cfe743ed3411851f7b3a5e244205db683fe67e
Ranieeery <raniery2003@hotmail.com>
2025-01-08 20:58:25 -0300

    feat: update chatbot service with additional product recommendation examples in Portuguese

--- src/main/java/dev/raniery/chatbot/domain/service/ChatbotService.java
+++ src/main/java/dev/raniery/chatbot/domain/service/ChatbotService.java
@@ -24,39 +24,6 @@ public class ChatbotService {
             Consider that we stock any and all products related in the store e-commerce description.
             If the customer asks for recommendations, reply with 2-3 recommendations based on what they asked for.
             Reply in the language of the user's last message.
-            
-            Example:
-            Question: Could you give me some recommendations for graphics cards?
-            Answer: Sure! Here are three graphics cards that are popular with our customers:
-            1. NVIDIA GeForce RTX 3080
-            2. AMD Radeon RX 6800 XT,
-            3. NVIDIA GeForce RTX 3060 Ti.
-            
-            Question: I'm having trouble with my order.
-            Answer: I'm sorry to hear that. Please contact our customer service team at customer@bytebox.com.br for assistance.
-            
-            Question: Which gaming chairs do you recommend
-            Answer: Here are two gaming chairs that are popular with our customers:
-            1. DXRacer Racing Series
-            2. Secretlab Omega.
-            
-            Question: Which cable do I connect the gpu to the motherboard?
-            Answer: You need a PCIe cable to connect the GPU to the motherboard.
-            
-            Question: How much is 2 + 2?
-            Answer: I'm sorry, I can only help with customer service questions and product recommendations.
-            
-            Question: opa, me indica umas placa de vídeo
-            Answer: Claro! Aqui estão três placas de vídeo que são populares entre nossos clientes:
-            1. NVIDIA GeForce RTX 3080
-            2. AMD Radeon RX 6800 XT,
-            3. NVIDIA GeForce RTX 4060 Ti.
-            
-            Question: Estou com problemas com meu pedido.
-            Answer: Sinto muito em ouvir isso. Entre em contato com nossa equipe de atendimento ao cliente em customer@bytebox.com.br para obter assistência.
-            
-            Question: Quem é o presidente do Brasil?
-            Answer: Desculpe, só posso ajudar com perguntas de atendimento ao cliente e recomendações de produtos.
             """;
         ChatCompletionDataRequest response = new ChatCompletionDataRequest(systemMessage, message);
         return client.sendRequestChatCompletion(response);
